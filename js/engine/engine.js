4906461587aecda7d92f4a6f4df5ce4d4d37f70b
Sándor Balázs <it.sandor.balazs@gmail.com>
2018-04-05 14:49:11 +0200

    A betöltéskezelés elkészült.

--- js/engine/engine.js
+++ js/engine/engine.js
@@ -1,6 +1,6 @@
 "use strict";
 class Game {
-    constructor(background, foreground, eventListener = new EventListener(), collisionHandler = new Collision()) {
+    constructor(background, foreground, eventListener = new EventListener(this)) {
         this.background             = document.getElementById(background);
         this.backgroundContext      = background.getContext('2d');
         this.foreground             = document.getElementById(foreground);
@@ -11,18 +11,18 @@ class Game {
         this.mouseListener          = new Map();
         this.costumEventListener    = new Map();
         this.eventListener          = eventListener;
-        this.collisionHandler       = collisionHandler;
+        this.collisionHandler       = new Collision(this);
+        this.fileLoader             = new FileLoader(this);
     }
     start() {
-        this.loader();
-        this.render(static);
+        this.fileLoader.load(this, loadFinished);
+        this.render("static");
         this.render();
     }
-    loader() {
-    }
-    staticRender() {
-    }
-    render(static = dynamic) {
+    render(mode = dynamic) {
+        if(mode = "static"){
+
+        }
     }
 
     addEntity(entity) {
@@ -67,14 +67,56 @@ class Game {
 }
 
 class EventListener {
-    constructor() {
-
+    constructor(game) {
+        this.game = game;
+    }
+    callEvent(event, value = null) {
     }
 }
 
 class Collision {
-    constructor() {
+    constructor(game) {
+        this.game = game;
+    }
+}
 
+class FileLoader {
+    constructor(game) {
+        this.game = game;
+        this.loadFinished = loadFinished;
+        this.callBackRegister = new Map();
+        this.checkLoadInterval = 35;
+        this.vaitingElements = 0;
+        this.allElements = 0;
+    }
+
+    load() {
+        for(let entity of game.staticEntitys) {
+            this.callBackRegister.set(entity, entity.load(this.callback));
+        }
+
+        for(let entity of game.diynamicEntitys) {
+            this.callBackRegister.set(entity, entity.load(this.callback));
+        }
+
+        for (let [entity, i] of this.callBackRegister) this.allElements += i;
+
+        var checkLoad = setInterval(function(){
+            for (let [entity, i] of this.callBackRegister) this.vaitingElements += i;
+
+            var progressInPercent = ((this.allElements-this.vaitingElements)/this.allElements)*100;
+            game.eventListener.callEvent("LoadInProgress", progressInPercent);
+
+            if(this.vaitingElements == 0) {
+                this.loadFinished();
+                game.eventListener.callEvent("LoadFinished");
+                clearInterval(checkLoad);
+            }
+        }, checkLoadInterval);
+    }
+
+    callback(entity) {
+        this.callBackRegister.set(entity, this.callBackRegister.get(entity)-1); 
     }
 }
 
@@ -87,6 +129,22 @@ class Entity {
         this.events = new Map();
         this.anima
     }
+    load(callBack) {
+        for (let image of this.images){
+            var hos = document.createElement('img');
+            hos.onload = callBack(this);
+            hos.src = image;
+        }
+        for (let sound of this.sounds){    
+            var sound = document.createElement('audio');
+            audio.oncanplay = callBack(this);
+            sound.src   = sound;
+        }
+        return this.images.size + this.sounds.size;
+    }
+    draw() {
+
+    }
     move() {
 
     }
@@ -98,6 +156,14 @@ class Entity {
     }
     hit() {
 
+    }
+    getImgX() {
+    }
+    getImgY() {
+    }
+    getWidth() {
+    }
+    getHeight() {
     }
 }
 
---
+++ js/hero.js
@@ -0,0 +1,9 @@
+mySet.add(1);
+mySet.add('some text');
+mySet.add('foo');
+
+mySet.has(1); // true
+mySet.delete('foo');
+mySet.size; // 2
+
+for (let item of mySet) console.log(item);
