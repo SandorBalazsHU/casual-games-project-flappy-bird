4896a16912f7259ee5f5f8b6e9e1266977ea180a
Sándor Balázs <it.sandor.balazs@gmail.com>
2018-04-05 11:54:51 +0200

    A Game osztály építése

--- js/engine/engine.js
+++ js/engine/engine.js
@@ -1,16 +1,17 @@
 "use strict";
 class Game {
-    constructor(background, foreground, eventListener = new eventListener()) {
+    constructor(background, foreground, eventListener = new EventListener(), collisionHandler = new Collision()) {
         this.background             = document.getElementById(background);
         this.backgroundContext      = background.getContext('2d');
         this.foreground             = document.getElementById(foreground);
         this.foregroundContext      = foreground.getContext('2d');
         this.staticEntitys          = new Set();
         this.diynamicEntitys        = new Set();
-        this.keyListener            = new Set();
-        this.mouseListener          = new Set();
-        this.costumEventListener    = new Set();
+        this.keyListener            = new Map();
+        this.mouseListener          = new Map();
+        this.costumEventListener    = new Map();
         this.eventListener          = eventListener;
+        this.collisionHandler       = collisionHandler;
     }
     start() {
         this.loader();
@@ -23,23 +24,58 @@ class Game {
     }
     render(static = dynamic) {
     }
+
     addEntity(entity) {
         if(entity instanceof Entity){
             if(typeof Entity.move === 'function')
             {
-                diynamicEntitys.add(entity);
+                this.diynamicEntitys.add(entity);
             }else{
-                staticEntitys.add(entity);
+                this.staticEntitys.add(entity);
             }
-            for (let item of mySet);
+            if(typeof Entity.event === 'function')
+            {
+                if(entity.events.get("key"))   this.keyListener.set(entity, entity.events.get("key"));
+                if(entity.events.get("mouse")) this.mouseListener.set(entity, entity.events.get("mouse"));
+                if(entity.events.get("event")) this.costumEventListener.set(entity, entity.events.get("event"));
+            }
+            if(typeof Entity.hit === 'function') collisionHandler.addEntity(entity);
+        }else{
+            new Error(typeof entity + " type is not Entity");
+        }
+    }
+
+    removeEntity(entity) {
+        if(entity instanceof Entity){
+            if(typeof Entity.move === 'function')
+            {
+                this.diynamicEntitys.delete(entity);
+            }else{
+                this.staticEntitys.delete(entity);
+            }
+            if(typeof Entity.event === 'function')
+            {
+                this.keyListener.delete(entity);
+                this.mouseListener.delete(entity);
+                this.costumEventListener.delete(entity);
+            }
+            if(typeof Entity.hit === 'function') collisionHandler.removeEntity(entity);
         }else{
             new Error(typeof entity + " type is not Entity");
         }
     }
 }
 
-class eventListener {
-    constructor()
+class EventListener {
+    constructor() {
+
+    }
+}
+
+class Collision {
+    constructor() {
+
+    }
 }
 
 class Entity {
@@ -48,16 +84,23 @@ class Entity {
         this.y      = y;
         this.images = new Set();
         this.sounds = new Set();
-        //["EventType", eventID]
-        this.events = new Set();
+        this.events = new Map();
+        this.anima
     }
-    move (){
+    move() {
 
     }
-    event (event){
+    event(event) {
 
     }
-    animate () {
+    animate() {
 
     }
+    hit() {
+
+    }
+}
+
+class diynamicEntity {
+
 }
